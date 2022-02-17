469aa46786447238f313d33c92127160db61e387
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-02-18 01:05:00 +0300

    Add the migrations

--- app/main/error.py
+++ app/main/error.py
@@ -1,9 +1,9 @@
-from flask import render_template
-from ..main import main
+# from flask import render_template
+# from app import app
 
-@main.errorhandler(404)
-def four_Ow_four(error):
-    '''
-    Function to render the 404 error page
-    '''
-    return render_template('fourOwfour.html'),404
+# @app.errorhandler(404)
+# def four_Ow_four(error):
+#     '''
+#     Function to render the 404 error page
+#     '''
+#     return render_template('fourOwfour.html'),404
