bc4925b3ba30adf3f7bbec978b54cba348340202
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-02-18 15:23:46 +0300

    Deployment to heroku

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     '''
     General configuration parent class
     ''' 
-    SQLALCHEMY_DATABASE_URI = 'postgres://yazapgyjbvarxf:0cbebb20130fbc21aa10981fcaaa57f447dbbaba3732c1bcf4c6d0e7c95dc898@ec2-54-209-221-231.compute-1.amazonaws.com:5432/d5ua106brjlq78'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://yazapgyjbvarxf:0cbebb20130fbc21aa10981fcaaa57f447dbbaba3732c1bcf4c6d0e7c95dc898@ec2-54-209-221-231.compute-1.amazonaws.com:5432/d5ua106brjlq78'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') 
     UPLOADED_PHOTOS_DEST = 'app/static/photos' 
@@ -22,7 +22,7 @@ class Config:
 
 class ProdConfig(Config):
   '''Production configuration class'''
-  SQLALCHEMY_DATABASE_URI = 'postgres://yazapgyjbvarxf:0cbebb20130fbc21aa10981fcaaa57f447dbbaba3732c1bcf4c6d0e7c95dc898@ec2-54-209-221-231.compute-1.amazonaws.com:5432/d5ua106brjlq78'
+  SQLALCHEMY_DATABASE_URI = 'postgresql://yazapgyjbvarxf:0cbebb20130fbc21aa10981fcaaa57f447dbbaba3732c1bcf4c6d0e7c95dc898@ec2-54-209-221-231.compute-1.amazonaws.com:5432/d5ua106brjlq78'
   # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
@@ -33,7 +33,7 @@ class DevConfig(Config):
     '''
     Development configuration child class 
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgres://yazapgyjbvarxf:0cbebb20130fbc21aa10981fcaaa57f447dbbaba3732c1bcf4c6d0e7c95dc898@ec2-54-209-221-231.compute-1.amazonaws.com:5432/d5ua106brjlq78'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://yazapgyjbvarxf:0cbebb20130fbc21aa10981fcaaa57f447dbbaba3732c1bcf4c6d0e7c95dc898@ec2-54-209-221-231.compute-1.amazonaws.com:5432/d5ua106brjlq78'
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:0000@localhost/pitch'
     DEBUG = True
 
